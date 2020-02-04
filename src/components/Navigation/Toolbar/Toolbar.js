5cef4129cd10f4219eb430d0a3ececadb448eaf7
Edvarda-Arnoldas Alaburda <edvardas.alaburda@ktu.edu>
2020-02-04 14:27:26 +0200

    base navigation implementation in toolbar

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import classes from './Toolbar.module.css';
 import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
 
 const Toolbar = () => {
     return (
@@ -8,11 +9,10 @@ const Toolbar = () => {
             <div>MENU</div>
             <Logo/>
             <nav>
-                ...
+                <NavigationItems/>
             </nav>
         </header>
     );
-
 };
 
 export default Toolbar;

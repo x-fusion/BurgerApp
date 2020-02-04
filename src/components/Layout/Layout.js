bf4510426ce4c4c38bd9507963fa1782da84d3c1
Edvarda-Arnoldas Alaburda <edvardas.alaburda@ktu.edu>
2020-02-04 10:17:15 +0200

    Layout update: implemented navigation bar and logo base

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
-import classes from './Layout.module.css'
+import Toolbar from "../Navigation/Toolbar/Toolbar";
+import classes from './Layout.module.css';
 
 const layout = (props) => (
     <Auxiliary>
         <div>
-            Toolbar, SideDrawer, Backdrop
+            <Toolbar/>
+            SideDrawer, Backdrop
         </div>
         <main className={classes.Container}>
             {props.children}
